2d3136c283238bed8928baf063a8f07565d35b63
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-05-25 02:41:48 +0700

    a

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -63,7 +63,9 @@ export function SignIn() {
       <h1>abc{cookies.token}</h1>
       <h1>{error}</h1>
       <div className="w-full lg:w-3/5 mt-24">
+
         <div className="text-center">
+          <h1>abc{cookies.token}</h1>
           <Typography variant="h2" className="font-bold mb-4">Sign In</Typography>
           <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Enter your email and password to Sign In.</Typography>
         </div>
